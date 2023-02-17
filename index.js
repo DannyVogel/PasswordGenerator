998c6bade910107284d9894645dfb16a992dfc1a
DannyVogel <danny.hj.vogel@gmail.com>
2023-02-17 11:39:49 +0100

    add copy-on-click and symbols/numbers exclusion option

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
 
 function renderPasswords() {
+    
     document.getElementById("password1").textContent = generatePassword()
     document.getElementById("password2").textContent = generatePassword()
 }
 
 function generatePassword() {
+    let excludeSymbols = document.getElementById("symbolsCheck").checked
+    let excludeNumbers = document.getElementById("numbersCheck").checked
+    let chosenCharacters = []
+    
+    if (excludeSymbols && excludeNumbers) {
+        chosenCharacters = letters
+    } else if (excludeNumbers) {
+        chosenCharacters = letters.concat(symbols)
+    } else if (excludeSymbols) {
+        chosenCharacters = letters.concat(numbers)
+    } else {
+        chosenCharacters = letters.concat(numbers, symbols)
+    }
+
     let password = ""
     let passwordLengthChoice = document.getElementById("passwordLength").value
     for (let i = 0; i < passwordLengthChoice; i++) {
-        let randomIndex = Math.floor(Math.random() * characters.length)
-        password += characters[randomIndex]
+        let randomIndex = Math.floor(Math.random() * chosenCharacters.length)
+        password += chosenCharacters[randomIndex]
     }
     
     return password
 }
 
-function toggleDark() {
-    document.getElementById("container").classList.toggle("light")
-}
-
-function updateLength() {
+function updatePasswordLength() {
     let passwordLengthChoice = document.getElementById("passwordLength").value
     document.getElementById("passwordLengthLabel").textContent = "Password length: " + passwordLengthChoice + " characters"
+}
+
+function copyPassword(event) {
+    navigator.clipboard.writeText(event.target.innerText)
+    
+    document.getElementById("passwordCopiedAlert").classList.toggle("hide")
+    setTimeout(() => {
+        document.getElementById("passwordCopiedAlert").classList.toggle("hide")
+    }, 1500)
+}
+
+function toggleDark() {
+    document.getElementById("container").classList.toggle("light")
 }
